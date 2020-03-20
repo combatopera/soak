30f8e538e48d4abc4715211a3e090538c213ebe6
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2020-03-20 13:24:32 +0000

    migrated to aridity

--- soak.py
+++ soak.py
@@ -93,10 +93,6 @@ def rootpath(context, *resolvables):
     root, = git.rev_parse.__show_toplevel().splitlines()
     return Text(str(Path(root, *(r.resolve(context).cat() for r in resolvables))))
 
-def hereslash(context, *resolvables):
-    here = context.resolved('here').cat()
-    return Text(str(Path(here, *(r.resolve(context).cat() for r in resolvables))))
-
 class Terminal:
 
     def __init__(self):
@@ -122,7 +118,6 @@ class SoakConfig:
     parent['xml"',] = Function(xmlquote)
     parent['|',] = Function(blockliteral)
     parent['//',] = Function(rootpath)
-    parent['./',] = Function(hereslash)
     del repl, f
 
     def __init__(self, configpath):
