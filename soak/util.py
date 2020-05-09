e0becb688ecb16adbe6b305f97c2b4d039dec95c
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2020-05-09 10:46:09 +0100

    also cache exception if any

--- soak/util.py
+++ soak/util.py
@@ -20,23 +20,42 @@ from threading import Lock
 
 class Snapshot(Resolvable):
 
+    class Result:
+
+        def __init__(self, eornone, obj):
+            self.eornone = eornone
+            self.obj = obj
+
+        def get(self):
+            if self.eornone is not None:
+                raise self.eornone
+            return self.obj
+
     def __init__(self, factory):
         self.lock = Lock()
         self.factory = factory
 
-    def _loadobj(self):
+    def _loadresult(self):
         with self.lock:
             try:
-                self.obj
+                self.result
             except AttributeError:
-                self.obj = self.factory()
+                f = self.factory
+                try:
+                    obj = f()
+                    eornone = None
+                except Exception as e:
+                    obj = None
+                    eornone = e
+                self.result = self.Result(eornone, obj)
 
     def resolve(self, context):
         try:
-            return self.obj
+            r = self.result
         except AttributeError:
-            self._loadobj()
-            return self.obj
+            self._loadresult()
+            r = self.result
+        return r.get()
 
 class PathResolvable(Resolvable):
 
